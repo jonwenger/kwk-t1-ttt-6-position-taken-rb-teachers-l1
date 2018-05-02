203ff4ca5387c536cb4020af19d263f87ab8ef4a
Jon Wenger <jonwenger783@gmail.com>
2018-05-02 19:36:01 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,14 @@
-# code your #position_taken? method here!
+# code your #position_taken? method here
+def position_taken?(board, index)
+##if board[index] == nil
+  ##false
+
+if board[index]== " " || board[index]== "" || board[index]==nil
+ false
+
+else
+ true
+
+end
+
+end
